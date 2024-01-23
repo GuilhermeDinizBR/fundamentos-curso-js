6c61fd4c31534c348d49673f5b71b2017fe4b212
GuilhermeDinizBR <guilhermedinizbr@gmail.com>
2024-01-23 13:10:36 -0300

    codigos var e conversoes

---
+++ boolean3.js
@@ -0,0 +1,18 @@
+// const usuarioLogado = true;
+// const contaPaga = false;
+
+// console.log(0 == false);
+// console.log('' == false);
+
+// console.log(1 == false);
+
+let minhaVar;
+let varNull = null;
+
+
+let numero = 3;
+let texto = 'Alura';
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+    
---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dados
+// boolean
+
+// conversao implicita
+const numero = 456;
+const numeroString = '456';
+
+// Number()
+// String()
+console.log(numero + Number(numeroString));
+
+
+
+// conversao explicita
